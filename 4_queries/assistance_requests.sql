2ec52f5d120de1b3d66c7cac03c6d4c59192a507
Mohammed <MMD.Mahdi21@gmail.com>
2022-06-08 23:34:32 +0000

    create 4_queries folder

---
+++ 4_queries/assistance_requests.sql
@@ -0,0 +1,7 @@
+/* Total Teacher Assistance Requests */
+
+SELECT count(assistance_requests.*) as total_assistances, teachers.name
+FROM assistance_requests JOIN teachers
+ON teacher_id = teachers.id
+WHERE name = 'Waylon Boehm'
+GROUP BY teachers.name;
